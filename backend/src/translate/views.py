4626d2f67a6b1e140221d70d4ca64917be772180
ilosrim <ilosrim@yahoo.com>
2021-12-04 21:00:55 +0500

    static files

--- backend/src/translate/views.py
+++ backend/src/translate/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'hello.html', {'ism': 'Mirsoli'})
+  return render(request, 'index.html', {'ism': 'Mirsoli'})
